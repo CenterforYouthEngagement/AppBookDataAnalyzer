92e4555d59fc20543ba9f63c968b8b7dc28b3ff5
Jeremy Kelleher <jkjak6@gmail.com>
2022-02-23 15:06:13 -0500

    Initial Commit

---
+++ AppBookDataAnalyzer/AppBookDataAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppBookDataAnalyzerApp.swift
+//  AppBookDataAnalyzer
+//
+//  Created by Jeremy Kelleher on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct AppBookDataAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
