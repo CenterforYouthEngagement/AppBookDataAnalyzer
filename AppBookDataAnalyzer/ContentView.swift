3b5b25ff095636a86251fb8417f0ba84c4b0fe90
Jeremy Kelleher <jkjak6@gmail.com>
2022-02-23 16:29:34 -0500

    draft for drop handling

--- AppBookDataAnalyzer/ContentView.swift
+++ AppBookDataAnalyzer/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let dropAreaLength: CGFloat = 300
+    
+    let dropDelegate = DirectoryDropDelegate()
+    
     var body: some View {
-        Text("Hello, world!")
+        Text("Drop iPad Documents Directory Here!")
+            .font(.title)
+            .multilineTextAlignment(.center)
+            .foregroundColor(.white)
             .padding()
+            .frame(width: dropAreaLength, height: dropAreaLength)
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.accentColor)
+                    .opacity(0.6)
+            )
+            .frame(width: dropAreaLength * 1.5, height: dropAreaLength * 1.5)
+            .onDrop(of: [.directory], delegate: dropDelegate)
+            
     }
 }
 
---
+++ AppBookDataAnalyzer/DirectoryDropDelegate.swift
@@ -0,0 +1,67 @@
+//
+//  DirectoryDropDelegate.swift
+//  AppBookDataAnalyzer
+//
+//  Created by Jeremy Kelleher on 2/23/22.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+class DirectoryDropDelegate: DropDelegate {
+    
+    var urlHandler: ((URL) -> Void)? = nil
+    
+    func performDrop(info: DropInfo) -> Bool {
+        
+        // TODO - maybe we can support dropping multiple iPad directories if we don't call `.first`
+        guard let directory = info.itemProviders(for: [.directory]).first else {
+            print("No directories found in the drag")
+            return false
+        }
+        
+        directory.loadItem(forTypeIdentifier: UTType.directory.identifier) { (urlData, error) in
+            
+            if let error = error {
+                print("Error loading directory: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let urlData = urlData as? Data else {
+                print("Data loaded from item provider couldn't be found")
+                return
+            }
+            
+            guard let url = URL(dataRepresentation: urlData, relativeTo: nil) else {
+                print("Couldn't find url in item provider's data")
+                return
+            }
+            
+            self.urlHandler?(url)
+            
+        }
+        
+        
+        return true
+        
+    }
+    
+    func validateDrop(info: DropInfo) -> Bool {
+        
+        print("New drop incoming...")
+        
+        guard info.hasItemsConforming(to: [.directory]) else {
+            print("It was a file")
+            return false
+        }
+        
+        print("It was a directory")
+        
+        return true
+        
+    }
+    
+    
+    
+    
+}
