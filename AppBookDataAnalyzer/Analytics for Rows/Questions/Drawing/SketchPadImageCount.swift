11f6a762d2285c1796428ace9a20aad12ed21897
Jeremy Kelleher <jkjak6@gmail.com>
2022-03-17 10:13:20 -0600

    move image analytics file

---
+++ AppBookDataAnalyzer/Analytics for Rows/Questions/Drawing/SketchPadImageCount.swift
@@ -0,0 +1,41 @@
+//
+//  SketchPadImageCount.swift
+//  AppBookDataAnalyzer
+//
+//  Created by Jeremy Kelleher on 3/17/22.
+//
+
+import GRDB
+import Foundation
+
+struct SketchPadImageCount: Analytic {
+        
+    var title: String = "Sketch Pad - Stroke Count - Image"
+    
+    func analyze(database: Database, textbookMaterial: TextbookMaterial) async -> String? {
+        
+        try? await database.pool.read { db in
+            
+            switch textbookMaterial {
+                
+            case .page(let appbook, let pageNumber):
+                
+                let query = Database.StrokeCollectionStroke.generateQuery(for: .image, appbookId: appbook.id, pageNumber: pageNumber)
+                
+                guard let count = try Int.fetchOne(db, sql: query) else {
+                    return nil
+                }
+                
+                return String(count)
+                
+                
+            case .job(_):
+                return nil
+            }
+            
+            
+        }
+        
+    }
+    
+}
