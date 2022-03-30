7346ca982b7c33fe40d8bef89d15c73414e4ac02
sdub18 <sdubois@umass.edu>
2022-03-30 00:20:03 -0400

    Created checklist analytic

---
+++ AppBookDataAnalyzer/Analytics for Rows/ChecklistNumItemsTapped.swift
@@ -0,0 +1,48 @@
+//
+//  ChecklistNumItemsTapped.swift
+//  AppBookDataAnalyzer
+//
+//  Created by Sam DuBois on 3/30/22.
+//
+
+import Foundation
+
+struct ChecklistNumOfItemsTapped: Analytic {
+    
+    let tableOfContentsEventCode = 00
+    
+    var title: String = "Checklist Number of Items Tapped"
+    
+    func analyze(database: Database, textbookMaterial: TextbookMaterial) async -> String? {
+        
+        try? await database.pool.read { db in
+            
+            switch textbookMaterial {
+                
+            case .page(let appbook, let pageNumber):
+                
+                let query = """
+                    SELECT COUNT(*)
+                    FROM \(Database.EventLog.tableName)
+                    WHERE \(Database.EventLog.Column.appbookId) = \(appbook.id)
+                    AND \(Database.EventLog.Column.pageNumber) = \(pageNumber)
+                    AND \(Database.EventLog.Column.code) = \(tableOfContentsEventCode)
+                """
+                
+                guard let count = try Int.fetchOne(db, sql: query) else {
+                    return nil
+                }
+                
+                return String(count)
+                
+                
+            case .job(_):
+                return nil
+            }
+            
+            
+        }
+        
+    }
+    
+}
