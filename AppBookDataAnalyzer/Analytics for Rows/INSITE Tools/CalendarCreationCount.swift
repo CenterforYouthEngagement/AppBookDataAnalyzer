71781d26f2de2cff850bad8d50a18d0959572a48
Frankie Furnelli <fjfur10@comcast.net>
2022-03-30 00:09:20 -0400

    create analytics

---
+++ AppBookDataAnalyzer/Analytics for Rows/INSITE Tools/CalendarCreationCount.swift
@@ -0,0 +1,35 @@
+//
+//  CalendarCreationCount.swift
+//  AppBookDataAnalyzer
+//
+//  Created by Francis Furnelli on 3/29/22.
+//
+
+import Foundation
+struct CalendarCreationCount: Analytic {
+    
+    let eventCode = 56//change
+    
+    var title: String = "Calendar Creation Count"
+    
+    func analyze(database: Database, textbookMaterial: TextbookMaterial) async -> String? {
+        
+        try? await database.pool.read { db in
+            
+            switch textbookMaterial {
+                
+            case .page(let appbook, let pageNumber):
+                
+                return try Database.count(eventCodes: [eventCode], appbookId: appbook.id, pageNumber: pageNumber, in: db)
+                
+            case .job(_):
+                return nil
+            }
+            
+            
+        }
+        
+    }
+    
+    
+}
