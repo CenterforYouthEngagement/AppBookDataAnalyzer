b05d742a3646b6814d53333f310069b0afb5d314
sdub18 <sdubois@umass.edu>
2022-03-30 17:51:56 -0400

    Resovled issue with database extension

--- AppBookDataAnalyzer/Analytics for Rows/ChecklistNumItemsTapped.swift
+++ AppBookDataAnalyzer/Analytics for Rows/ChecklistNumItemsTapped.swift
@@ -23,7 +23,14 @@ struct ChecklistNumOfItemsTapped: Analytic {
                 
                 let query = """
                     SELECT COUNT(*)
-                    FROM 
+                    FROM \(Database.)
+                    FROM checklist_selected_items
+                    JOIN checklist_items
+                    ON checklist_items.id = checklist_selected_items.checklist_item_id
+                    JOIN question_to_page_join
+                    ON question_to_page_join.question_id = checklist_items.question_id
+                    WHERE question_to_page_join.question_id = 1613
+                    AND question_to_page_join.appbook_id = 91
                 """
                 
                 guard let count = try Int.fetchOne(db, sql: query) else {
--- AppBookDataAnalyzer/Database/Question/Database+ChecklistStudentAnswer.swift
+++ AppBookDataAnalyzer/Database/Question/Database+ChecklistStudentAnswer.swift
@@ -7,23 +7,26 @@
 
 import Foundation
 
-struct ChecklistItems {
-    static let tableName = "checklist_items"
-    struct Column {
-        static let id = "id"
-        static let questionId = "question_id"
-        static let description = "description"
-        static let imageId = "image_id"
-        static let rank = "rank"
+extension Database {
+    
+    struct ChecklistItems {
+        static let tableName = "checklist_items"
+        struct Column {
+            static let id = "id"
+            static let questionId = "question_id"
+            static let description = "description"
+            static let imageId = "image_id"
+            static let rank = "rank"
+        }
     }
-}
 
-struct ChecklistSelectedItems {
-    static let tableName = "checklist_selected_items"
-    struct Column {
-        static let id = "id"
-        static let checklistItemId = "checklist_item_id"
-        static let isSelected = "is_selected"
-        static let timestamp = "timestamp"
+    struct ChecklistSelectedItems {
+        static let tableName = "checklist_selected_items"
+        struct Column {
+            static let id = "id"
+            static let checklistItemId = "checklist_item_id"
+            static let isSelected = "is_selected"
+            static let timestamp = "timestamp"
+        }
     }
 }
