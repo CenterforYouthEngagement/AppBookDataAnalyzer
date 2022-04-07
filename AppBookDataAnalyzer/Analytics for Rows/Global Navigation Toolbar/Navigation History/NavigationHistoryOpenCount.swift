c203c31df73b4848164f04a203d1f29ec7190dc5
Jeremy Kelleher <jkjak6@gmail.com>
2022-04-06 21:07:40 -0600

    expand navigation history event code analysis

---
+++ AppBookDataAnalyzer/Analytics for Rows/Global Navigation Toolbar/Navigation History/NavigationHistoryOpenCount.swift
@@ -0,0 +1,33 @@
+//
+//  NavigationHistoryOpenCount.swift
+//  AppBookDataAnalyzer
+//
+//  Created by Jeremy Kelleher on 3/28/22.
+//
+
+import Foundation
+
+struct NavigationHistoryToolbarOpenCount: Analytic {
+        
+    var title: String = "Navigation History Open Count - Toolbar"
+    
+    func analyze(database: Database, textbookMaterial: TextbookMaterial) async -> String? {
+        
+        try? await database.pool.read { db in
+            
+            switch textbookMaterial {
+                
+            case .page(let appbook, let pageNumber):
+                
+                return try Database.count(events: [.navigationHistoryOpenedFromGlobalToolbar], appbookId: appbook.id, pageNumber: pageNumber, in: db)
+                
+            case .job(_):
+                return nil
+            }
+            
+            
+        }
+        
+    }
+    
+}
