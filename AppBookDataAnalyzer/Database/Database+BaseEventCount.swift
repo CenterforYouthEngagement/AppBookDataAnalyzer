3bc5357eff1af7ed7d386a3ca022e0b959d0627f
Jeremy Kelleher <jkjak6@gmail.com>
2022-04-08 18:43:54 -0600

    update docs

--- AppBookDataAnalyzer/Database/Database+BaseEventCount.swift
+++ AppBookDataAnalyzer/Database/Database+BaseEventCount.swift
@@ -13,7 +13,7 @@ extension Database {
     
     /// Basic `SELECT COUNT(*)` that counts the `event_log` for rows where `eventCodes` occured in the given `appbookId` on the `pageNumber`. Single  `eventCodes` arrays are support. When `eventCodes.count > 1`, an `OR` statement will be used when checking for each of the `eventCodes` in `event_log`
     /// - Parameters:
-    ///   - eventCodes: An array of `Int` event codes. Reference can be found [here](https://www.notion.so/80fee0b450414d659f184187f1ea81ef?v=a9cd8a5c7aad46799272b1d731f591d2)
+    ///   - events: An array of `AppBookAnalyticEvent` events. Reference can be found [here](https://github.com/CenterforYouthEngagement/AppBookAnalyticEvents/blob/main/Sources/AppBookAnalyticEvents/AppBookAnalyticEvent.swift)
     ///   - appbookId: The `AppBook.ID`, most likely present in the `TextbookMaterial` passed in during the `anaylze` function for an `Analytic`
     ///   - pageNumber: The `pageNumber` in the `AppBook`, most likely present in the `TextbookMaterial` passed in during the `anaylze` function for an `Analytic`
     ///   - database: A reference to the `GRDB.Database` to run the query on. See `TableOfContentsOpened.swift:19` for how to get a reference to the database.
